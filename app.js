a6c3ec28153ba9e433e4d52e87600be56dc7ce20
Ammar Khan <khan.ammar6k@gmail.com>
2021-12-07 22:54:55 +0530

    added footer

--- app.js
+++ app.js
@@ -20,14 +20,63 @@ function closeMenu() {
  navBar.classList.remove("active");
 }
 
-// demo
-$(document).ready(function () {
- $("#owl-demo").owlCarousel({
-  autoPlay: 3000, //Set AutoPlay to 3 seconds
+// coding with nick testimonial
+// vars
+("use strict");
+var testim = document.getElementById("testim"),
+ testimDots = Array.prototype.slice.call(
+  document.getElementById("testim-dots").children
+ ),
+ testimContent = Array.prototype.slice.call(
+  document.getElementById("testim-content").children
+ ),
+ testimleftArrow = document.getElementById("left-arrow"),
+ testimRightArrow = document.getElementById("right-arrow"),
+ testimSpeed = 5000,
+ currentSlide = 0,
+ currentActive = 0,
+ testimTimer;
+// coding with nick
+window.onload = function () {
+ // Testim Script
+ function playSlide(slide) {
+  for (var k = 0; k < testimDots.length; k++) {
+   testimContent[k].classList.remove("active");
+   testimContent[k].classList.remove("inactive");
+   testimDots[k].classList.remove("active");
+  }
+  if (slide < 0) {
+   slide = currentSlide = testimContent.length - 1;
+  }
+  if (slide > testimContent.length - 1) {
+   slide = currentSlide = 0;
+  }
+  if (currentActive != currentSlide) {
+   testimContent[currentActive].classList.add("inactive");
+  }
+  testimContent[slide].classList.add("active");
+  testimDots[slide].classList.add("active");
 
-  items: 4,
-  itemsDesktop: [1199, 3],
-  itemsDesktopSmall: [979, 3],
+  currentActive = currentSlide;
+
+  clearTimeout(testimTimer);
+  testimTimer = setTimeout(function () {
+   playSlide((currentSlide += 1));
+  }, testimSpeed);
+ }
+ // coding with nick
+ testimleftArrow.addEventListener("click", function () {
+  playSlide((currentSlide -= 1));
  });
-});
-// demo
+ testimRightArrow.addEventListener("click", function () {
+  playSlide((currentSlide += 1));
+ });
+
+ for (var l = 0; l < testimDots.length; l++) {
+  testimDots[l].addEventListener("click", function () {
+   playSlide((currentSlide = testimDots.indexOf(this)));
+  });
+ }
+ playSlide(currentSlide);
+};
+// coding with nick
